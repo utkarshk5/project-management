765620fd20d0f8c3981eac602065c37094d3c323
Utkarsh Kumar <kumar.utkarsh007@gmail.com>
2015-10-31 07:15:50 +0530

    sessions and login shit working!

---
+++ src/database/Submit.java
@@ -0,0 +1,106 @@
+package database;
+
+import java.io.IOException;
+import java.sql.Date;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class Submit
+ */
+@WebServlet("/Submit")
+public class Submit extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public Submit() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		System.out.println("entered submit servlet");
+		String formType = null;
+		if(request.getParameterMap().containsKey("formType")){
+			System.out.println("formtype was set");
+			formType=request.getParameter("formType");
+		}
+		else {
+			request.getRequestDispatcher("Login").forward(request, response);
+			return;
+		}
+
+		System.out.println(formType);
+		switch(formType){
+			case "addUser":
+				User.addUser(
+					request.getParameter("name"), request.getParameter("email"),
+					request.getParameter("password"), request.getParameter("phone"),
+					request.getParameter("address"), Integer.parseInt(request.getParameter("team_id")));
+				request.setAttribute("error", "Registered successfully! Please login");
+				request.getRequestDispatcher("Login").forward(request, response);
+				return;
+
+			case "createSubtask":
+	//			Task.createSubTask(request.getParameter("assignedTo"),
+	//				request.getParameter("title"),
+	//				request.getParameter("deadline"),
+	//				Integer.parseInt(request.getParameter("supertask")),
+	//				Integer.parseInt(request.getParameter("assigner_id")));
+				System.out.println(request.getParameter("assignedTo"));
+				break;
+			
+			
+			case "extendDeadline":
+				Task.extendDeadline(
+						Integer.parseInt(request.getParameter("id")),
+						Date.valueOf(request.getParameter("date")) );
+				break;
+			
+			case "createTask":
+				
+				break;
+	
+			case "deleteUser":
+				break;
+				
+			case "makeTeam":
+				Team.makeTeam(request.getParameter("team_name"), Integer.parseInt(request.getParameter("leader_id")));
+				break;
+			
+			case "changeTeamLeader": 
+				break;
+			
+			case "deleteTeam": 
+				User.deleteUser(Integer.parseInt(request.getParameter("id")));
+				break;
+			
+			case "addMemberToTeam": 
+				break;
+					
+		}
+		System.out.println("reached the end, to be forwarded to login servlet!");
+		request.getRequestDispatcher("Login").forward(request, response);
+		return;
+
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
